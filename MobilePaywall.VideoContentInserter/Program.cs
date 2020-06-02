7cdfe6ea7828522bcc93025463985d6a0ef896a5
aco228 <35331284+aco228@users.noreply.github.com>
2020-06-02 22:15:37 +0200

    Initial commit

---
+++ MobilePaywall.VideoContentInserter/Program.cs
@@ -0,0 +1,31 @@
+﻿using MobilePaywall.ContentUploader.Video;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePaywall.VideoContentInserter
+{
+	class Program
+	{
+		// PATH TO THE CONTENT FOLDER
+		public static string CONTENT_FOLDER = @"C:\Users\ako\Desktop\upload lorenc";
+		// this folder must have "_config.txt" with prefiled informations
+
+		static void Main(string[] args)
+		{
+			MobilePaywall.Data.Sql.Database db = null;
+			Senti.Data.DataLayerRuntime runtime = new Senti.Data.DataLayerRuntime();
+			VideoUploaderManager manager = new VideoUploaderManager(@"C:\Users\ako\Desktop\upload lorenc");
+			manager.Start();
+
+			Console.ReadKey();
+		}
+		
+
+
+
+	}
+}
